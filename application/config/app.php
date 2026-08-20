f9de28ba8e08469dcf833e25e251b8db36cb1485
romdoniahmad185-cmyk <romdoniahmad185@gmail.com>
2026-08-21 00:49:13 +0700

    Add files via upload

---
+++ application/config/app.php
@@ -0,0 +1,8 @@
+<?php
+return [
+ 'name'=>'MTA School System',
+ 'environment'=>'production',
+ 'base_url'=>'http://localhost/mta',
+ 'timezone'=>'Asia/Jakarta',
+ 'version'=>'1.0.0'
+];
---
+++ application/config/database.php
@@ -0,0 +1,8 @@
+<?php
+return [
+ 'host'=>'localhost',
+ 'database'=>'if0_42655866_mta',
+ 'username'=>'CHANGE_DATABASE_USER',
+ 'password'=>'CHANGE_DATABASE_PASSWORD',
+ 'charset'=>'utf8mb4'
+];
